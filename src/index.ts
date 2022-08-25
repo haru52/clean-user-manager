df90a53f5f849c156e10ed6a6579dd6c69e2c724
haru <haru@haru52.com>
2022-08-25 23:16:44 +0900

    chore: update shebang

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import ConsoleUi from './ui/console-ui';
 import ConsoleUserCreatePresenter from './interface-adapters/presenters/user/console-user-create-presenter';
