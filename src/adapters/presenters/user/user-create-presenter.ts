22fa7f116dc40b18abceee5b945ccdfa50470090
haru <haru@haru52.com>
2022-08-28 17:36:08 +0900

    refactor: refactor user-create-presenter.ts

--- src/adapters/presenters/user/user-create-presenter.ts
+++ src/adapters/presenters/user/user-create-presenter.ts
@@ -1,16 +1,15 @@
-import { ViewModel } from '../../view-model';
 import { UserCreateOutputBoundary } from '../../../use-cases/user/create/user-create-output-boundary';
 import { UserCreateOutputData } from '../../../use-cases/user/create/user-create-output-data';
 import { View } from '../../view';
+import { ViewModel } from '../../view-model';
 
 export default class UserCreatePresenter implements UserCreateOutputBoundary {
-  #view: View;
+  readonly #view: View;
 
   constructor(view: View) {
     this.#view = view;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   present(outputData: UserCreateOutputData) {
     const viewModel: ViewModel = {
       message: `User “${outputData.name}” has been created with ID ${outputData.id} successfully!`,
