ce1248988ba62624fefe3e9fcb6cc0e5f46a7d71
haru <haru@haru52.com>
2022-08-29 17:29:47 +0900

    chore: add TSDoc comments

--- src/adapters/repositories/user-repository.ts
+++ src/adapters/repositories/user-repository.ts
@@ -1,7 +1,31 @@
 import User from '../../entities/user';
 
 export interface UserRepository {
+  /**
+   * Save a new user to the DB.
+   *
+   * @param name - User name to save
+   * @returns Saved {@link User} instance
+   *
+   * @throws NotFoundError
+   * This exception is thrown if the user is not found after the insertion of a new user to the DB.
+   *
+   * @throws Error
+   * This exception is thrown if the DB throws an unexpected error.
+   */
   save(name: string): Promise<User>;
+  /**
+   * Find a user from the DB.
+   *
+   * @param id - User ID to find
+   * @returns Found {@link User} instance
+   *
+   * @throws NotFoundError
+   * This exception is thrown if the user is not found.
+   *
+   * @throws Error
+   * This exception is thrown if the DB throws an unexpected error.
+   */
   find(id: number): Promise<User>;
   close(): void;
 }
