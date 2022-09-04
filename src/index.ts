57060f90a08fc5d2d68aab16b4271fd2ac927aa8
haru <haru@haru52.com>
2022-09-04 17:57:11 +0900

    refactor: rename isError to isErr

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ import SqliteDbConnector from './adapters/repositories/sqlite-db-connector';
 
 async function main() {
   const consoleUi = container.resolve(ConsoleUi);
-  let isError = false;
+  let isErr = false;
   await consoleUi.handle().catch(() => {
-    isError = true;
+    isErr = true;
   });
 
   const dbConnector = container.resolve(SqliteDbConnector);
   dbConnector.close();
 
-  if (isError) process.exit(1);
+  if (isErr) process.exit(1);
 }
 
 main();
